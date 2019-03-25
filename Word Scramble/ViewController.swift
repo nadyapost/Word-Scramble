8f6646d175ad5b736c8b845a766636fe13cdaf78
nadyapost <nadyapostr@gmail.com>
2019-03-25 21:13:39 +1100

    Check for valid answers methods.

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -15,9 +15,10 @@ class ViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptAnswer))
     if let startWordsListURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
       if let startWords = try? String(contentsOf: startWordsListURL) {
-        allWords = startWords.components(separatedBy: "/n")
+        allWords = startWords.components(separatedBy: "\n")
         
       }
     }
@@ -31,6 +32,7 @@ class ViewController: UITableViewController {
     usedWords.removeAll(keepingCapacity: true)
     tableView.reloadData()
   }
+  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return usedWords.count
   }
@@ -39,6 +41,71 @@ class ViewController: UITableViewController {
     cell.textLabel?.text = usedWords[indexPath.row]
     return cell
   }
+  @objc func promptAnswer() {
+    let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+    ac.addTextField()
+    let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+      guard let answer = ac?.textFields?[0].text else {return}
+      self?.submit(answer)
+    }
+    ac.addAction(submitAction)
+    present(ac, animated: true)
+  }
   
+  func submit(_ answer: String) {
+    let lowerAnswer = answer.lowercased()
+    let errorTitle: String
+    let errorMessage: String
+    
+    if isPosible(word: lowerAnswer) {
+      if isOriginal(word: lowerAnswer) {
+        if isReal(word: lowerAnswer) {
+          usedWords.insert(answer, at: 0)
+          
+          let indexPath = IndexPath(row: 0, section: 0)
+          tableView.insertRows(at: [indexPath], with: .automatic)
+          return
+        } else {
+          errorTitle = "Word not recognised"
+          errorMessage = "You can't just make them up, you know!"
+        }
+      } else {
+        errorTitle = "Word used already"
+        errorMessage = "Be more original!"
+      }
+    } else {
+      errorTitle = "Word not possible"
+      errorMessage = "You can't spell that word from \(title!.lowercased())"
+    }
+    let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "Ok", style: .default))
+    present(ac, animated: true)
+  }
+  
+  func isPosible(word: String) -> Bool {
+    guard var tempWord = title?.lowercased() else { return false }
+    
+    for letter in word {
+      if let position = tempWord.firstIndex(of: letter) {
+        tempWord.remove(at: position)
+      } else {
+        return false
+      }
+    }
+    return true
+  }
+  
+  func isOriginal(word: String) -> Bool {
+    return !usedWords.contains(word)
+  }
+  func isReal(word: String) -> Bool {
+    let checker = UITextChecker()
+    let range = NSRange(location: 0, length: word.utf16.count)
+    let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+    
+    return misspelledRange.location == NSNotFound
+  }
+  
+
 }
 
