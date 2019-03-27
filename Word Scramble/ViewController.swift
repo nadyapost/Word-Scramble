276822c12995fac66778cfb4d40ccae9caa291b8
nadyapost <nadyapostr@gmail.com>
2019-03-27 21:22:27 +1100

    Submit only lowercased answers.

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -47,19 +47,18 @@ class ViewController: UITableViewController {
     let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
     ac.addTextField()
     let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
-      guard let answer = ac?.textFields?[0].text else {return}
-      self?.submit(answer)
+      guard let answer = ac?.textFields?[0].text else { return }
+      self?.submit(answer.lowercased())
     }
     ac.addAction(submitAction)
     present(ac, animated: true)
   }
   
   func submit(_ answer: String) {
-    let lowerAnswer = answer.lowercased()
     
-    if isPosible(word: lowerAnswer) {
-      if isOriginal(word: lowerAnswer) {
-        if isReal(word: lowerAnswer) {
+    if isPosible(word: answer) {
+      if isOriginal(word: answer) {
+        if isReal(word: answer) {
           usedWords.insert(answer, at: 0)
           
           let indexPath = IndexPath(row: 0, section: 0)
@@ -93,8 +92,9 @@ class ViewController: UITableViewController {
   func isOriginal(word: String) -> Bool {
     return !usedWords.contains(word)
   }
+  
   func isReal(word: String) -> Bool {
-    if word.count <= 3 || word == title {
+    if word.count < 3 || word == title {
       return false
     }
     let checker = UITextChecker()
